8e43d2b2658e2796269d0c26b244d50e6f5690e3
csro23ei <dante030724@gmail.com>
2024-05-16 10:31:08 +0200

    Har fixat det mesta (tror jag)

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,36 @@
-function Register() {
-    return (
-        <div>
-            
-        </div>
-    );
+import React, { useState } from 'react';
+
+function RegisterForm({ handleRegistration }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Registreringsuppgifter: ', { username, password });
+    handleRegistration();
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Användarnamn:</label>
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        <label>Lösenord:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <button type="submit">Registrera</button>
+    </form>
+  );
 }
 
-export default Register;
+export default RegisterForm;
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,13 @@
-function Start() {
-    return (
-        <div>
-            
-        </div>
-    );
+import React from 'react';
+
+function Start({ goToLogin, goToRegister }) {
+  return (
+    <div>
+      <h1>startsidan</h1>
+      <button onClick={goToLogin}>Gå till inloggning</button>
+      <button onClick={goToRegister}>Registrera</button>
+    </div>
+  );
 }
 
 export default Start;
