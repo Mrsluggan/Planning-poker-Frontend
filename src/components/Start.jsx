67da786207dbfa152bf76af5eb22abc3ffcb8195
mrsluggan <ericflyger@gmail.com>
2024-05-27 14:21:10 +0200

    Design för projektsidan

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function Start({ goToLogin, goToRegister }) {
   return (
     <div className="backgroundContainer">
-    <div className="mainContainer">
+    <div className="mainContainer" style={{textAlign: 'center'}}>
         {/* Här lägger du till ditt innehåll för startsidan */}
         <div className="titleContainer">
             <h1>startsidan</h1>
             <p> Välkomen till grupp 3 planing project site</p>
         </div>
-        <div className="buttonContainer">
+        <div className="buttonContainer" style={{display: 'flex', gap: '40px'}}>
             <button onClick={goToLogin}>Gå till inloggning</button>
             <button onClick={goToRegister}>Registrera</button>
         </div>
